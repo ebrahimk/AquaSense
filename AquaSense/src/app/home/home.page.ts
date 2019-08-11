a2a1244ba6bdd6bb876792c2109c5c7e6ff0e05f
Kamron <ebrahimikam@gmail.com>
2019-08-11 14:15:57 +0200

    setting up bluetooth LE connection

---
+++ AquaSense/src/app/home/home.page.ts
@@ -0,0 +1,91 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  unpairedDevices: any;
+  pairedDevices: any;
+  gettingDevices: boolean;
+  constructor(private bluetoothSerial: BluetoothSerial, public alertController: AlertController) {
+    bluetoothSerial.enable();
+  }
+
+  startScanning() {
+    this.pairedDevices = null;
+    this.unpairedDevices = null;
+    this.gettingDevices = true;
+    this.bluetoothSerial.discoverUnpaired().then((success) => {
+      this.unpairedDevices = success;
+      this.gettingDevices = false;
+      success.forEach(element => {
+        // alert(element.name);
+      });
+    },
+      (err) => {
+        console.log(err);
+      });
+
+    this.bluetoothSerial.list().then((success) => {
+      this.pairedDevices = success;
+    },
+      (err) => {
+
+      });
+  }
+  success = (data) => alert(data);
+  fail = (error) => alert(error);
+
+  async selectDevice(address: any) {
+    const alert = await this.alertController.create({
+      header: 'Connect',
+      subHeader: 'Subtitle',
+      message: 'Do you want to connect with?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Connect',
+          handler: () => {
+            this.bluetoothSerial.connect(address).subscribe(this.success, this.fail);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async disconnect() {
+    const alert = await this.alertController.create({
+      header: 'Disconnect?',
+      message: 'Do you want to Disconnect?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Disconnect',
+          handler: () => {
+            this.bluetoothSerial.disconnect();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+}
